d2867dde6665653de476c35f4481d33d3f4b7818
M A R M <seanbrucet@gmail.com>
2019-11-27 22:03:02 -0600

    First Commit

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,37 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+    <Icon class="icon" v-b-modal.modal-center/>
+    <b-modal id="modal-center" centered>
+      <p class="mb-0">This is a logo</p>
+      <div class="bg-dark">
+        <object type="image/svg+xml" data="/assets/logos/Group.svg" class="logo"></object>
+      </div>
+    </b-modal>
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator';
-import HelloWorld from './components/HelloWorld.vue';
+import Icon from './components/Mask.vue';
 
 @Component({
   components: {
-    HelloWorld,
+    Icon
   },
 })
 export default class App extends Vue {}
 </script>
 
 <style lang="scss">
+.icon {
+  margin: 50vh auto;
+}
+
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
---
+++ src/components/Mask.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="wrapper">
+        <div class="mask">
+            
+        </div>
+    </div>
+</template>
+
+<style lang="scss">
+    .wrapper {
+        &:hover {
+            cursor: pointer;
+        }
+        background:tomato;
+        width:20px;
+        height: 20px;
+    }
+</style>
+
+<script>
+export default {
+    name: "Icon"
+}
+</script>
