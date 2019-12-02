45c496e8fc2cd93200d6018f1bcdf902a20e5407
M A R M <seanbrucet@gmail.com>
2019-12-01 22:02:33 -0600

    WIP: Including modal diamonds on map

--- src/App.vue
+++ src/App.vue
@@ -1,183 +1,270 @@
 <template>
-  <div id="app">
+    <div id="app">
 
-    <div class="container-fluid">
-      <div class="row no-wrap align-center align-center">
-        <div class="col-sm">
-          <img class="img-fluid " src="./assets/logos/1.svg" alt="logo">
+        <div class="container-fluid" v-on:click="openMap">
+                <h1 class="display-3 main-title" v-bind:class="{'fade-in': isMapOpen }"> Toj' me me</h1>
+            <div class="row no-wrap align-center align-center transition">
+                <div class="col-sm" v-bind:class="{'img-1': isMapOpen }">
+                    <img  v-bind:class="{'img-not-so-fluid': isMapOpen}" class="img-fluid control img-1" src="./assets/logos/1.svg" alt="logo" ref="test">
+                </div>
+                <div class="col-sm delay-2" v-bind:class="{'img-2': isMapOpen }">
+                    <img class="img-fluid" src="./assets/logos/2.svg" alt="logo">
+                    <img class="img-fluid pt-2" src="./assets/logos/2.svg" alt="logo">
+                </div>
+                <div class="col-sm" v-bind:class="{'img-3': isMapOpen }">
+                    <img class="img-fluid" src="./assets/logos/3.svg" alt="logo">
+                </div>
+                <div class="col-sm delay-1" v-bind:class="{'img-4': isMapOpen }">
+                    <img class="img-fluid" src="./assets/logos/4.svg" alt="logo">
+                </div>
+                <div class="col-sm" v-bind:class="{'img-5': isMapOpen }">
+                    <img class="img-fluid" src="./assets/logos/5.svg" alt="logo">
+                </div>
+                <div class="col-sm delay-2" v-bind:class="{'img-6': isMapOpen }">
+                    <img class="img-fluid" src="./assets/logos/6.svg" alt="logo">
+                </div>
+                <div class="col-sm" v-bind:class="{'img-7': isMapOpen }">
+                    <img class="img-fluid" src="./assets/logos/7.svg" alt="logo">
+                </div>
+                <div class="col-sm delay-1" v-bind:class="{'img-8': isMapOpen }">
+                    <img class="img-fluid" src="./assets/logos/8.svg" alt="logo">
+                </div>
+                <div class="col-sm" v-bind:class="{'img-9': isMapOpen }">
+                    <img class="img-fluid" src="./assets/logos/9.svg" alt="logo">
+                </div>
+                <div class="col-sm delay-2" v-bind:class="{'img-10': isMapOpen }">
+                    <img class="img-fluid" src="./assets/logos/10.svg" alt="logo">
+                </div>
+            </div>
         </div>
-        <div class="col-sm delay-1">
-            <img class="img-fluid" src="./assets/logos/2.svg" alt="logo">
-            <img class="img-fluid" src="./assets/logos/2.svg" alt="logo">
-        </div>
-        <div class="col-sm">
-            <img class="img-fluid" src="./assets/logos/3.svg" alt="logo">
-        </div>
-         <div class="col-sm delay-2">
-            <img class="img-fluid" src="./assets/logos/4.svg" alt="logo">
-        </div>
-         <div class="col-sm">
-            <img class="img-fluid" src="./assets/logos/5.svg" alt="logo">
-        </div>
-         <div class="col-sm delay-1">
-            <img class="img-fluid" src="./assets/logos/6.svg" alt="logo">
-        </div>
-         <div class="col-sm">
-            <img class="img-fluid" src="./assets/logos/7.svg" alt="logo">
-        </div>
-         <div class="col-sm delay-2">
-            <img class="img-fluid" src="./assets/logos/8.svg" alt="logo">
-        </div>
-         <div class="col-sm">
-            <img class="img-fluid" src="./assets/logos/9.svg" alt="logo">
-        </div>
-         <div class="col-sm delay-1">
-            <img class="img-fluid" src="./assets/logos/10.svg" alt="logo">
-        </div>
-      </div>
-  </div>
 
-    <b-modal id="modal-center" centered>
-        <img src="./assets/logos/8.svg" alt="">
-        <h4 class="my-1">Titulo</h4>
-        <div class="d-flex">
-          <div class="d-grid--left">
-            <p class="text-left">Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium non velit porro error vel magni, autem tenetur ex nemo qui eum officiis praesentium possimus, quas quidem dolores animi placeat. Dolorum!</p>
-            <img src="https://via.placeholder.com/150x150" alt="">
+        <b-modal id="modal-center" centered>
+            <img src="./assets/logos/8.svg" alt="">
+            <h4 class="my-1">Titulo</h4>
+            <div class="d-flex">
+                <div class="d-grid--left">
+                    <p class="text-left">Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium non velit
+                        porro error vel magni, autem tenetur ex nemo qui eum officiis praesentium possimus, quas quidem
+                        dolores animi placeat. Dolorum!</p>
+                    <img src="https://via.placeholder.com/150x150" alt="">
 
-          </div>
-          <div class="d-grid--right">
-            <img src="https://via.placeholder.com/150x150" alt="">
-            <p class="text-left">Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium non velit porro error vel magni, autem tenetur ex nemo qui eum officiis praesentium possimus, quas quidem dolores animi placeat. Dolorum!</p>
+                </div>
+                <div class="d-grid--right">
+                    <img src="https://via.placeholder.com/150x150" alt="">
+                    <p class="text-left">Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium non velit
+                        porro error vel magni, autem tenetur ex nemo qui eum officiis praesentium possimus, quas quidem
+                        dolores animi placeat. Dolorum!</p>
 
-          </div>
-        </div>
-    </b-modal>
-  </div>
+                </div>
+            </div>
+        </b-modal>
+    </div>
 </template>
 
-<script lang="ts">
-import { Component, Vue } from 'vue-property-decorator';
-import Icon from './components/Mask.vue';
+<script lang="js">
+    import {Component, Vue} from 'vue-property-decorator';
+    import Icon from './components/Mask.vue';
 
-@Component({
-  components: {
-    Icon
-  },
-})
-export default class App extends Vue {}
+    @Component({
+        components: {
+            Icon
+        },
+        data() {
+            return {
+                isMapOpen : false,
+        }
+        },
+        methods: {
+            openMap() {
+                this.isMapOpen = true;
+            },
+        }
+    })
+    export default class App extends Vue {
+
+    }
+
 </script>
 
 <style lang="scss">
-#app {
-  height: 100%;
-}
+    #app {
+        height: 100%;
+    }
 
-.container-fluid {
-  height:inherit;
-}
+    .container-fluid {
+        height: inherit;
+    }
 
-.row {
-  height: 100%;
-}
+    .row {
+        height: 100%;
+    }
 
-.align-center {
-  align-content: center;
-}
+    .align-center {
+        align-content: center;
+    }
 
-.img-fluid {
-	-webkit-animation: vibrate-2 0.75s ease-out 2s infinite both;
-	        animation: vibrate-2 0.75s ease-out 2s infinite both;
-}
+    .img-fluid {
+        -webkit-animation: vibrate-2 900ms ease-out 2s infinite both;
+        animation: vibrate-2 900ms ease-out 2s infinite both;
+    }
 
-.delay-1 {
-  	-webkit-animation: vibrate-2 0.75s ease-out 1s infinite both;
-	        animation: vibrate-2 0.75s ease-out 1s infinite both;
-}
+    .transition > * {
+        transition: all 1200ms ease-out;
+    }
 
-.delay-2 {
-  -webkit-animation: vibrate-2 0.75s ease-out 3s infinite both;
-	        animation: vibrate-2 0.75s ease-out 3s infinite both;
-}
+    /*.delay-1 {*/
+    /*    -webkit-animation: vibrate-2 850ms ease-in  infinite both;*/
+    /*    animation: vibrate-2 850ms ease-in infinite both;*/
+    /*}*/
+
+    /*.delay-2 {*/
+    /*    -webkit-animation: vibrate-2 800ms ease-in-out infinite both;*/
+    /*    animation: vibrate-2 800ms ease-in-out infinite both;*/
+    /*}*/
 
 
-@-webkit-keyframes vibrate-2 {
-  0% {
-    -webkit-transform: translate(0);
+    @-webkit-keyframes vibrate-2 {
+        0% {
+            -webkit-transform: translate(0);
             transform: translate(0);
-  }
-  20% {
-    -webkit-transform: translate(2px, -2px);
+        }
+        20% {
+            -webkit-transform: translate(2px, -2px);
             transform: translate(2px, -2px);
-  }
-  40% {
-    -webkit-transform: translate(2px, 2px);
+        }
+        40% {
+            -webkit-transform: translate(2px, 2px);
             transform: translate(2px, 2px);
-  }
-  60% {
-    -webkit-transform: translate(-2px, 2px);
+        }
+        60% {
+            -webkit-transform: translate(-2px, 2px);
             transform: translate(-2px, 2px);
-  }
-  80% {
-    -webkit-transform: translate(-2px, -2px);
+        }
+        80% {
+            -webkit-transform: translate(-2px, -2px);
             transform: translate(-2px, -2px);
-  }
-  100% {
-    -webkit-transform: translate(0);
+        }
+        100% {
+            -webkit-transform: translate(0);
             transform: translate(0);
-  }
-}
-@keyframes vibrate-2 {
-  0% {
-    -webkit-transform: translate(0);
+        }
+    }
+
+    @keyframes vibrate-2 {
+        0% {
+            -webkit-transform: translate(0);
             transform: translate(0);
-  }
-  20% {
-    -webkit-transform: translate(2px, -2px);
+        }
+        20% {
+            -webkit-transform: translate(2px, -2px);
             transform: translate(2px, -2px);
-  }
-  40% {
-    -webkit-transform: translate(2px, 2px);
+        }
+        40% {
+            -webkit-transform: translate(2px, 2px);
             transform: translate(2px, 2px);
-  }
-  60% {
-    -webkit-transform: translate(-2px, 2px);
+        }
+        60% {
+            -webkit-transform: translate(-2px, 2px);
             transform: translate(-2px, 2px);
-  }
-  80% {
-    -webkit-transform: translate(-2px, -2px);
+        }
+        80% {
+            -webkit-transform: translate(-2px, -2px);
             transform: translate(-2px, -2px);
-  }
-  100% {
-    -webkit-transform: translate(0);
+        }
+        100% {
+            -webkit-transform: translate(0);
             transform: translate(0);
-  }
-}
+        }
+    }
 
 
-.d-flex > * {
-  border:1px solid white;
-}
+    .d-flex > * {
+        border: 1px solid white;
+    }
 
-.d-flex  {
-  border: 1px solid #f1f1f1;
-  .d-grid--right {
-    flex: 1;
-  }
-  .d-grid--left {
-    flex:3;
-  }
-}
+    .d-flex {
+        border: 1px solid #f1f1f1;
 
-.icon {
-  margin: 50vh auto;
-}
+        .d-grid--right {
+            flex: 1;
+        }
 
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #fff;
-}
+        .d-grid--left {
+            flex: 3;
+        }
+    }
+
+    .icon {
+        margin: 50vh auto;
+    }
+
+    #app {
+        font-family: 'Avenir', Helvetica, Arial, sans-serif;
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+        text-align: center;
+        color: #fff;
+    }
+
+    .col-sm {
+        display: inline-block;
+    }
+
+    .img-1 {
+         // transform: translateY(28em);
+     }
+
+    .img-2 {
+        transform: translateY(28em);
+    }
+
+    .img-3 {
+        transform: translateY(20em);
+    }
+
+    .img-4 {
+        transform: translateY(12em);
+    }
+
+    .img-5 {
+        transform: translateY(26em);
+    }
+
+    .img-6 {
+        // transform: translateY(24em);
+    }
+
+    .img-7 {
+        transform: translateY(18em);
+    }
+
+    .img-8 {
+        transform: translateY(7em);
+    }
+
+    .img-9 {
+        transform: translateY(-17em);
+    }
+
+    .img-10 {
+        // transform: translateY(-17em);
+    }
+
+    .main-title {
+        position: absolute;
+        margin: 0 auto;
+        margin-top: 0px;
+        text-align: center;
+        width: 100%;
+        margin-top: 2em;
+        opacity: 0;
+        transition: all 4200ms ease-out;
+        font-family: 'Kavivanar', cursive;
+    }
+
+    .fade-in {
+        opacity: 1;
+    }
+
+
 </style>
