724540c3da9e4e1428f281a5fe1c1e387c6f4a7e
M A R M <seanbrucet@gmail.com>
2019-12-03 23:48:30 -0600

    WIP: Including modal diamonds on map

--- src/App.vue
+++ src/App.vue
@@ -4,66 +4,90 @@
         <div class="container-fluid" v-on:click="openMap">
             <h1 class="display-3 main-title" v-bind:class="{'fade-in': isMapOpen }"> Toj̓ ̓ me me </h1>
             <div class="d-grid">
-                <div class="col-1">1</div>
-                <div class="col-2">2</div>
-                <div class="col-3">3</div>
-                <div class="col-1">1</div>
-                <div class="col-2">2</div>
-                <div class="col-3">3</div>
-                <div class="col-1">1</div>
-                <div class="col-2">2</div>
-                <div class="col-3">3</div>
+                <div class="diamond first-diamond" v-bind:class="{'fade-in': isMapOpen }" v-b-modal.modal-1></div>
+                <div class=""></div>
+                <div class=""></div>
+                <div class=""></div>
+                <div class="diamond sixth-diamond" v-bind:class="{'fade-in': isMapOpen }"></div>
+                <div class=""></div>
+                <div class="diamond third-diamond" v-bind:class="{'fade-in': isMapOpen }"></div>
+                <div class=""></div>
+                <div class="diamond fifth-diamond" v-bind:class="{'fade-in': isMapOpen }"></div>
+                <div class=""></div>
+                <div class="diamond second-diamond" v-bind:class="{'fade-in': isMapOpen }"></div>
+                <div class=""></div>
+                <div class="diamond fourth-diamond" v-bind:class="{'fade-in': isMapOpen }"></div>
+                <div class=""></div>
+                <div class="diamond seventh-diamond" v-bind:class="{'fade-in': isMapOpen }"></div>
             </div>
             <div class="row no-wrap align-center align-center transition">
                 <div class="col-sm" v-bind:class="{'img-1': isMapOpen }">
-                    <img v-bind:class="{'img-not-so-fluid': isMapOpen}" class="img-fluid control img-1"
-                         src="./assets/logos/1.svg" alt="logo" ref="test">
+                    <div>
+                        <img class="img-fluid animate-logo" src="./assets/logos/1.svg" alt="logo" ref="test">
+                    </div>
                 </div>
-                <div class="col-sm delay-2" v-bind:class="{'img-2': isMapOpen }">
-                    <img class="img-fluid" src="./assets/logos/2.svg" alt="logo">
-                    <img class="img-fluid pt-2" src="./assets/logos/2.svg" alt="logo">
+                <div class="col-sm" v-bind:class="{'img-2': isMapOpen }">
+                    <div>
+                        <img class="img-fluid animate-logo delay-2" src="./assets/logos/2.svg" alt="logo">
+                        <img class="img-fluid animate-logo delay-2" src="./assets/logos/2.svg" alt="logo">
+                    </div>
                 </div>
                 <div class="col-sm" v-bind:class="{'img-3': isMapOpen }">
-                    <img class="img-fluid" src="./assets/logos/3.svg" alt="logo">
+                    <div>
+                        <img class="img-fluid animate-logo" src="./assets/logos/3.svg" alt="logo">
+                    </div>
                 </div>
-                <div class="col-sm delay-1" v-bind:class="{'img-4': isMapOpen }">
-                    <img class="img-fluid" src="./assets/logos/4.svg" alt="logo">
+                <div class="col-sm" v-bind:class="{'img-4': isMapOpen }">
+                    <div>
+                        <img class="img-fluid animate-logo delay-1" src="./assets/logos/4.svg" alt="logo">
+                    </div>
                 </div>
                 <div class="col-sm" v-bind:class="{'img-5': isMapOpen }">
-                    <img class="img-fluid" src="./assets/logos/5.svg" alt="logo">
+                    <div>
+                        <img class="img-fluid animate-logo" src="./assets/logos/5.svg" alt="logo">
+                    </div>
                 </div>
-                <div class="col-sm delay-2" v-bind:class="{'img-6': isMapOpen }">
-                    <img class="img-fluid" src="./assets/logos/6.svg" alt="logo">
+                <div class="col-sm" v-bind:class="{'img-6': isMapOpen }">
+                    <div>
+                        <img class="img-fluid animate-logo delay-2" src="./assets/logos/6.svg" alt="logo">
+                    </div>
                 </div>
                 <div class="col-sm" v-bind:class="{'img-7': isMapOpen }">
-                    <img class="img-fluid" src="./assets/logos/7.svg" alt="logo">
+                    <div>
+                        <img class="img-fluid animate-logo" src="./assets/logos/7.svg" alt="logo">
+                    </div>
                 </div>
-                <div class="col-sm delay-1" v-bind:class="{'img-8': isMapOpen }">
-                    <img class="img-fluid" src="./assets/logos/8.svg" alt="logo">
+                <div class="col-sm" v-bind:class="{'img-8': isMapOpen }">
+                    <div>
+                        <img class="img-fluid animate-logo delay-1" src="./assets/logos/8.svg" alt="logo">
+                    </div>
                 </div>
                 <div class="col-sm" v-bind:class="{'img-9': isMapOpen }">
-                    <img class="img-fluid" src="./assets/logos/9.svg" alt="logo">
+                    <div>
+                        <img class="img-fluid animate-logo" src="./assets/logos/9.svg" alt="logo">
+                    </div>
                 </div>
-                <div class="col-sm delay-2" v-bind:class="{'img-10': isMapOpen }">
-                    <img class="img-fluid" src="./assets/logos/10.svg" alt="logo">
+                <div class="col-sm" v-bind:class="{'img-10': isMapOpen }">
+                    <div>
+                        <img class="img-fluid animate-logo delay-2" src="./assets/logos/10.svg" alt="logo">
+                    </div>
                 </div>
             </div>
         </div>
 
-        <b-modal id="modal-center" centered>
-            <img src="./assets/logos/8.svg" alt="">
-            <h4 class="my-1">Titulo</h4>
+        <b-modal id="modal-1" centered>
+            <img class="modal-logo" src="./assets/logos/8.svg" alt="">
+            <h3 class="my-1 mb-4 font-weight-bold text-uppercase">Titulo</h3>
             <div class="d-flex">
                 <div class="d-grid--left">
-                    <p class="text-left">Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium non velit
+                    <p class="text-left p-2">Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium non velit
                         porro error vel magni, autem tenetur ex nemo qui eum officiis praesentium possimus, quas quidem
                         dolores animi placeat. Dolorum!</p>
-                    <img src="https://via.placeholder.com/150x150" alt="">
 
                 </div>
                 <div class="d-grid--right">
                     <img src="https://via.placeholder.com/150x150" alt="">
-                    <p class="text-left">Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium non velit
+                    <p class="text-left p-2">Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium non velit
                         porro error vel magni, autem tenetur ex nemo qui eum officiis praesentium possimus, quas quidem
                         dolores animi placeat. Dolorum!</p>
 
@@ -115,7 +139,7 @@
         align-content: center;
     }
 
-    .img-fluid {
+    .animate-logo {
         -webkit-animation: vibrate-2 900ms ease-out 2s infinite both;
         animation: vibrate-2 900ms ease-out 2s infinite both;
     }
@@ -124,15 +148,15 @@
         transition: all 1200ms ease-out;
     }
 
-    /*.delay-1 {*/
-    /*    -webkit-animation: vibrate-2 850ms ease-in  infinite both;*/
-    /*    animation: vibrate-2 850ms ease-in infinite both;*/
-    /*}*/
+    .delay-1 {
+        -webkit-animation: vibrate-2 850ms ease-in infinite both;
+        animation: vibrate-2 850ms ease-in infinite both;
+    }
 
-    /*.delay-2 {*/
-    /*    -webkit-animation: vibrate-2 800ms ease-in-out infinite both;*/
-    /*    animation: vibrate-2 800ms ease-in-out infinite both;*/
-    /*}*/
+    .delay-2 {
+        -webkit-animation: vibrate-2 800ms ease-in-out infinite both;
+        animation: vibrate-2 800ms ease-in-out infinite both;
+    }
 
 
     @-webkit-keyframes vibrate-2 {
@@ -210,6 +234,10 @@
         margin: 50vh auto;
     }
 
+    .fish-offset {
+        transform: translateY(1em) !important;
+    }
+
     #app {
         font-family: 'Avenir', Helvetica, Arial, sans-serif;
         -webkit-font-smoothing: antialiased;
@@ -223,43 +251,50 @@
     }
 
     .img-1 {
-        // transform: translateY(28em);
+        opacity: .2;
     }
 
     .img-2 {
         transform: translateY(28em);
+        opacity: .2;
     }
 
     .img-3 {
         transform: translateY(20em);
+        opacity: .2;
     }
 
     .img-4 {
         transform: translateY(12em);
+        opacity: .2;
     }
 
     .img-5 {
         transform: translateY(26em);
+        opacity: .2;
     }
 
     .img-6 {
-        // transform: translateY(24em);
+        opacity: .2;
     }
 
     .img-7 {
         transform: translateY(18em);
+        opacity: .2;
     }
 
     .img-8 {
         transform: translateY(7em);
+        opacity: .2;
     }
 
     .img-9 {
         transform: translateY(-17em);
+        opacity: .2;
     }
 
     .img-10 {
-        // transform: translateY(-17em);
+        opacity: .2;
     }
 
     .main-title {
@@ -275,16 +310,77 @@
     }
 
     .fade-in {
-        opacity: 1;
+        opacity: 1 !important;
     }
 
     .d-grid {
         display: grid;
-        grid-template-columns: 1fr 1fr 1fr;
+        grid-template-columns: 1fr 1fr 1fr 1fr 1fr;
         height: 100%;
         position: absolute;
-        opacity: .1;
+        opacity: .8;
         width: 100%;
+    }
+
+    .diamond {
+        opacity: 0;
+        width: 2em;
+        height: 2em;
+        cursor: pointer;
+        transform: rotate(45deg);
+        border-radius: 10px;
+        background: #ffffff6e;
+        transition: all 3800ms ease-in;
+
+        &:hover {
+            transition: none;
+            background: white;
+        }
+    }
+
+    .first-diamond {
+        align-self: end;
+        justify-self: flex-end;
+    }
+
+    .second-diamond {
+        justify-self: end;
+    }
+
+    .third-diamond {
+        justify-self: end;
+        align-self: center;
+    }
+
+    .fourth-diamond {
+        justify-self: center;
+        align-self: center;
+    }
+
+    .fifth-diamond {
+        align-self: center;
+    }
+
+    .sixth-diamond {
+        align-self: end;
+    }
+
+    .seventh-diamond {
+
+    }
+
+    .modal-content {
+        background: #ddd !important;
+    }
+
+    .modal-logo {
+        vertical-align: middle;
+        border-style: none;
+        background: #484848;
+        height: 120px;
+        width: 120px;
+        border-radius: 50%;
+        padding: 10px;
     }
 
 
